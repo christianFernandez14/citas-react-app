c42485e3ee3fa281c4e6324b524beb0b1378d4f0
Christian Fernandez <christian14@gmail.com>
2023-12-15 10:57:44 -0300

    Creando Botones de editar y eleminar para cada carta de pacientes (81)

--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -8,7 +8,7 @@ const Paciente = ({ paciente }) => {
 
     <div
       className="m-3 bg-white shadow-md px-5 py-10 rounded-lg ">
-        
+
       <p
         className="font-bold mb-3 text-gray-700 uppercase">
         nombre: {''}
@@ -53,6 +53,24 @@ const Paciente = ({ paciente }) => {
           {sintoma}
         </span>
       </p>
+
+      <div
+        className="flex justify-between mt-10">
+
+        <button
+          type="button"
+          className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white uppercase font-bold rounded-lg"
+        >
+          editar
+        </button>
+
+        <button
+          type="button"
+          className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white uppercase font-bold rounded-lg"
+        >
+          eliminar
+        </button>
+      </div>
 
     </div>
 
